25ebfc6dcceb79595ca7b5f1316957ebe37aa6ce
Dzhemilya <Dzhemilya.giz@yandex.ru>
2022-04-01 20:10:58 +0300

    added creation of database file, but used psql for work

---
+++ week06/db_creation.sql
@@ -0,0 +1,13 @@
+createdb mydb;
+psql mydb
+  --creating tables 
+create table Suppliers (sid int NOT NULL, sname varchar(255) NOT NULL, address varchar(255) NOT NULL);
+create table Suppliers (sid int NOT NULL, sname varchar(255) NOT NULL, address varchar(255) NOT NULL);
+create table Suppliers (sid int NOT NULL, sname varchar(255) NOT NULL, address varchar(255) NOT NULL);
+--insert values into tables
+INSERT INTO Suppliers (sid, sname, address) VALUES (1, 'Yosemite Sham', E'Devil\'s canyon, AZ'), (2, 'Whiley E.Coyote', 'RR Asylum, NV'), (3, 'Elmer Fudd', 'Carrot Patch, MN');
+ 
+INSERT INTO Parts (pid, pname, color) VALUES (1, 'Red1', 'Red'), (2, 'Red2', 'Red'), (3, 'Green1', 'Green'), (4, 'Blue1', 'Blue'), (5, 'Red3', 'Red');
+  
+INSERT INTO Catalog (sid, pid, cost) VALUES (1, 1, 10), (1, 2, 20), (1, 3, 30), (1, 4, 40), (1, 5, 50), (2, 1, 9), (2, 3, 34), (2, 5, 48);
+
