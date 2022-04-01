dc508c623458063751dda4639fa02adcaf340c3a
Dzhemilya <Dzhemilya.giz@yandex.ru>
2022-04-01 21:17:58 +0300

    edited creation

--- week06/db_creation.sql
+++ week06/db_creation.sql
@@ -2,8 +2,8 @@ createdb mydb;
 psql mydb
   --creating tables 
 create table Suppliers (sid int NOT NULL, sname varchar(255) NOT NULL, address varchar(255) NOT NULL);
-create table Suppliers (sid int NOT NULL, sname varchar(255) NOT NULL, address varchar(255) NOT NULL);
-create table Suppliers (sid int NOT NULL, sname varchar(255) NOT NULL, address varchar(255) NOT NULL);
+CREATE TABLE IF NOT EXISTS Parts (pid int NOT NULL PRIMARY KEY, pname varchar(255) NOT NULL, color varchar(255) NOT NULL);
+CREATE TABLE IF NOT EXISTS Catalog (sid integer, pid integer, cost float NOT NULL, primary key(sid, pid));
 --insert values into tables
 INSERT INTO Suppliers (sid, sname, address) VALUES (1, 'Yosemite Sham', E'Devil\'s canyon, AZ'), (2, 'Whiley E.Coyote', 'RR Asylum, NV'), (3, 'Elmer Fudd', 'Carrot Patch, MN');
  
